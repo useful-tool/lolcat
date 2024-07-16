7fc6e889646c2c4e0ed94806192e973af68534bb
180909 <734461790@qq.com>
2024-07-16 23:45:56 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/useful-tool/lolcat
+
+go 1.22.1
---
+++ lolcat.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+)
+
+func rgb(i int) (int, int, int) {
+	var f = 0.1
+	return int(math.Sin(f*float64(i)+0)*127 + 128),
+		int(math.Sin(f*float64(i)+2*math.Pi/3)*127 + 128),
+		int(math.Sin(f*float64(i)+4*math.Pi/3)*127 + 128)
+}
+
+func print(output []rune) {
+	if len(output) <= 0 {
+		return
+	}
+	for j := 0; j < len(output); j++ {
+		r, g, b := rgb(j)
+		fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, output[j])
+	}
+	fmt.Println()
+}
+
+func main() {
+	info, _ := os.Stdin.Stat()
+	var output []rune
+
+	if info.Mode()&os.ModeCharDevice != 0 {
+		fmt.Println("The command is intended to work with pipes.")
+		fmt.Println("Usage: fortune | gorainbow")
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		input, _, err := reader.ReadRune()
+		if err != nil && err == io.EOF {
+			break
+		}
+		output = append(output, input)
+	}
+
+	print(output)
+}
